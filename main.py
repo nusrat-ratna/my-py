b5bf37108b80a814981f1d7fa92814edb1763138
Nusrat Jahan <nusrat.ratna@gmai.com>
2025-07-12 18:18:13 +0600

    dictionary

---
+++ main.py
@@ -0,0 +1,18 @@
+xinfo = {
+    "Tamjid":{
+
+    "nana":"tamjid",
+    "address":"Dhaka"
+
+},
+   "ratna":{
+    "nana":"tamjid",
+    "address":"Dhaka",
+       "roll":'34',
+    "class": "8"
+},
+    "year":"1221"
+}
+xinfo["year"]="43"
+xinfo["ratna"]["class"]=10
+print(xinfo["ratna"]["class"])
